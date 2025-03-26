691dc4cba16672a8dc07642009532cf87d2536cd
osamaghamdi <ghamdios235@gmail.com>
2025-03-26 23:46:06 +0000

    add file print_d.c

---
+++ print_d.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+int print_decimal(va_list args)
+{
+int num = va_arg(args, int);
+char buffer[12];
+int len = sprintf(buffer, "%d", num);
+return (write(1, buffer, len));
+}
