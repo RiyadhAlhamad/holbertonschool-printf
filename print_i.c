c4c0ebd919d0170218542c9022249aa1694916ba
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 05:30:01 +0300

    Update print_i.c

--- print_i.c
+++ print_i.c
@@ -12,5 +12,5 @@ int print_int(va_list args)
 int num = va_arg(args, int);
 char buffer[12];
 int len = sprintf(buffer, "%d", num);
-return write(1, buffer, len);
+return (write(1, buffer, len));
 }
