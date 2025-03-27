10ccb0e60144301e85be641bc370879d2842d545
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 04:06:27 +0300

    edit some issu with betty style print_c.c

--- print_c.c
+++ print_c.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_char - Prints a single character
- * @args: Argument list containing the character to print
- *
- * Return: Number of characters printed (always 1)
- */
+* print_char - Prints a single character
+* @args: Argument list containing the character to print
+*
+* Return: Number of characters printed (always 1)
+*/
 int print_char(va_list args)
 {
 	char c = va_arg(args, int)
