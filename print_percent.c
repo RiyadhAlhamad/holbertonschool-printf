bc24296d3001373d8003550558da250c47d8b6b1
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 04:37:14 +0300

    Update print_percent.c

--- print_percent.c
+++ print_percent.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include <unistd.h>
 /**
 * print_percent - Prints a percent symbol (%)
@@ -6,8 +5,7 @@
 *
 * Return: Number of characters printed (always 1)
 */
-int print_percent(va_list args)
+int print_percent(void)
 {
-	(void)args;
 	return write(1, "%", 1);
 }
