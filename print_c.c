da8dd1c115fce326dfcbb47cf4af43a819a35aef
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 03:59:32 +0300

    add commint with betty style print_c.c

--- print_c.c
+++ print_c.c
@@ -1,7 +1,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-
+/**
+ * print_char - Prints a single character
+ * @args: Argument list containing the character to print
+ *
+ * Return: Number of characters printed (always 1)
+ */
 int print_char(va_list args)
 {
 	char c = va_arg(args, int)
