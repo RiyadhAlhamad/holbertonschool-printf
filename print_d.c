47d40c100700ea7e888e72b16d246ea5aaf6a547
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 04:01:40 +0300

    add commint with betty style print_d.c

--- print_d.c
+++ print_d.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "main.h"
-
+/**
+* print_decimal - Prints an integer in decimal format
+* @args: Argument list containing the integer to print
+*
+* Return: Number of characters printed
+*/
 int print_decimal(va_list args)
 {
 int num = va_arg(args, int);
