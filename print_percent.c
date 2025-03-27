9d5075a0ed58e1b14e62f8683248426415dff2dc
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-03-27 04:04:25 +0300

    add commit with betty style print_percent.c

--- print_percent.c
+++ print_percent.c
@@ -1,7 +1,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-
+/**
+* print_percent - Prints a percent symbol (%)
+* @args: Argument list (unused)
+*
+* Return: Number of characters printed (always 1)
+*/
 int print_percent(va_list args)
 {
 	(void)args;
